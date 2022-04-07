2e455f51db3e770341e35f9507987b6d6ea92f81
Danil Strelyaev <danil.strelyaev@gmail.com>
2022-04-07 14:48:07 +0300

    v 1.0.0 - all done, some optimizing to do

--- src/main/java/com/example/examplemod/TreeCapitatorEvents.java
+++ src/main/java/com/example/examplemod/TreeCapitatorEvents.java
@@ -15,76 +15,117 @@ import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import java.util.HashSet;
+import java.util.Iterator;
+
 @Mod.EventBusSubscriber
 public class TreeCapitatorEvents {
 
     @SubscribeEvent
-        public static void onBlockBreak(PlayerEvent.BreakSpeed event){
+        public static void onBlockBreaking(PlayerEvent.BreakSpeed event){
             event.getEntityPlayer().sendMessage(new TextComponentString(String.valueOf(event.getOriginalSpeed())));
-            event.getEntityPlayer().sendMessage(new TextComponentString(String.valueOf(woodBreakingRecursionCount(event.getPos(), event.getEntityPlayer().getEntityWorld(), event.getState(), 0))));
-                //event.setNewSpeed(event.getOriginalSpeed() - woodBreakingRecursionCount(event.getPos(), event.getEntityPlayer().getEntityWorld(), event.getState()) * 0.1f);
-                //event.getEntityPlayer().sendMessage(new TextComponentString(String.valueOf(woodBreakingRecursionCount(event.getPos(), event.getEntityPlayer().getEntityWorld(), event.getState()))));
-                //event.getEntityPlayer().sendMessage(new TextComponentString(String.valueOf(event.getNewSpeed())));
+                if (event.getEntityPlayer().getHeldItemMainhand().getItem() instanceof ItemAxe) {
+                    event.getEntityPlayer().sendMessage(new TextComponentString("Axe have been used"));
+                    if (event.getEntityPlayer().isSneaking() == true) {
+                        event.getEntityPlayer().sendMessage(new TextComponentString("Player is sneaking"));
+                        if ( event.getEntityPlayer().getEntityWorld().getBlockState(event.getPos()).getBlock() == Blocks.LOG ||  event.getEntityPlayer().getEntityWorld().getBlockState(event.getPos()).getBlock() == Blocks.LOG2) {
 
-                if (event.getEntityPlayer().getHeldItemMainhand().getItem() instanceof ItemAxe) { //вот тут кринж, работает только с объектом в мэйн хенде, по другому я не знаю как лист преобразовать в один элемент, .get() не работает
-                //if (event.getPlayer().getHeldEquipment() instanceof ItemAxe){
-                event.getEntityPlayer().sendMessage(new TextComponentString("Axe have been used"));
+                            HashSet<BlockPos> blocksToBeBroken = new HashSet<BlockPos>();
+                            blocksToBeBroken.add(event.getPos());
+                            blocksToBeBroken = woodBreakingRecursionCount(event.getPos(),event.getEntityPlayer().getEntityWorld(),event.getState(),blocksToBeBroken);
 
-                //if(event.)
+                            event.setNewSpeed(event.getOriginalSpeed() / blocksToBeBroken.size());
+                            event.getEntityPlayer().sendMessage(new TextComponentString("HashSet Size:"+blocksToBeBroken.size()));
+                            event.getEntityPlayer().sendMessage(new TextComponentString("Current speed "+event.getNewSpeed()));
 
-                if (event.getEntityPlayer().isSneaking() == true) {
-                    event.getEntityPlayer().sendMessage(new TextComponentString("Player is sneaking"));
-                    if ( event.getEntityPlayer().getEntityWorld().getBlockState(event.getPos()).getBlock() == Blocks.LOG ||  event.getEntityPlayer().getEntityWorld().getBlockState(event.getPos()).getBlock() == Blocks.LOG2) {
-                        event.getEntityPlayer().sendMessage(new TextComponentString("Block broken"));
-                        woodBreakingRecursion(event.getPos(),  event.getEntityPlayer().getEntityWorld(), event.getState(), event.getEntityPlayer(), 0);
+                            event.getEntityPlayer().sendMessage(new TextComponentString("Current result "+event.getResult()));
 
-                        //System.out.println("hello block");
+                            //event.
+
+                            //woodBreaking(event.getPos(), event.getEntityPlayer().getEntityWorld(), event.getState(), event.getEntityPlayer(), blocksToBeBroken);
+                            //event.getEntityPlayer().sendMessage(new TextComponentString("Block broken"));
                     }
                 }
             }
         }
 
-        //Не работает, бесконечный цикл
-    /*private static float woodBreakingRecursionCount(BlockPos pos, World world, IBlockState state, float step) {
-        step++;
-        for (int y = 0; y <= 1; y++) { //stackoverflow
+
+        @SubscribeEvent
+        public static void onBlockBreak(BlockEvent.BreakEvent event) {
+            //event.getEntityPlayer().sendMessage(new TextComponentString(String.valueOf(event.getOriginalSpeed())));
+            if (event.getPlayer().getHeldItemMainhand().getItem() instanceof ItemAxe) {
+                event.getPlayer().sendMessage(new TextComponentString("Axe have been used"));
+                if (event.getPlayer().isSneaking() == true) {
+                    event.getPlayer().sendMessage(new TextComponentString("Player is sneaking"));
+                    if ( event.getWorld().getBlockState(event.getPos()).getBlock() == Blocks.LOG ||  event.getWorld().getBlockState(event.getPos()).getBlock() == Blocks.LOG2) {
+
+                        //HashSet<BlockPos> blocksToBeBroken = new HashSet<BlockPos>();
+                        //blocksToBeBroken.add(event.getPos());
+                       //blocksToBeBroken = woodBreakingRecursionCount(event.getPos(),event.getEntityPlayer().getEntityWorld(),event.getState(),blocksToBeBroken);
+
+                        //event.setNewSpeed(event.getOriginalSpeed() / blocksToBeBroken.size());
+                        //event.getEntityPlayer().sendMessage(new TextComponentString("HashSet Size:"+blocksToBeBroken.size()));
+                        //event.getEntityPlayer().sendMessage(new TextComponentString("Current speed"+event.getNewSpeed()));
+
+                        HashSet<BlockPos> blocksToBeBroken = new HashSet<BlockPos>();
+                        blocksToBeBroken.add(event.getPos());
+                        //woodBreaking(event.getPos(), event.getWorld(), event.getState(), event.getPlayer(), woodBreakingRecursionCount(event.getPos(),event.getWorld(),event.getState(),blocksToBeBroken));
+                        woodBreaking(event.getPos(), event.getWorld(), event.getState(), event.getPlayer(), 0);
+                        event.getPlayer().sendMessage(new TextComponentString("Block broken"));
+
+                    }
+                }
+            }
+        }
+
+
+    private static HashSet woodBreakingRecursionCount(BlockPos pos, World world, IBlockState state, HashSet<BlockPos> knownPos) {
+        for (int y = 0; y <= 1; y++) {
             for (int x = -1; x <= 1; x++) {
                 for (int z = -1; z <= 1; z++) {
                     if ((world.getBlockState(pos.add(x,y,z)).getBlock() == Blocks.LOG || world.getBlockState(pos.add(x,y,z)).getBlock() == Blocks.LOG2) && pos.add(x,y,z) != pos) {
-                        step = woodBreakingRecursionCount(pos.add(x,y,z), world, state, step);
-                        if(step >= 30) {
-                            return step;
+                        if (knownPos.size() == 30) {
+                            return knownPos;
+                        }
+                        if(!(knownPos.contains(pos.add(x,y,z)))) {
+                            knownPos.add(pos.add(x,y,z));
+                            knownPos = woodBreakingRecursionCount(pos.add(x,y,z), world, state, knownPos);
                         }
                     }
                 }
             }
         }
-        return step;
-    }*/
+        return knownPos;
+    }
 
-        private static void woodBreakingRecursion(BlockPos pos, World world, IBlockState state, EntityPlayer player, int step) {
-            step++;
+        //private static void woodBreaking(BlockPos pos, World world, IBlockState state, EntityPlayer player, HashSet knownPos, int step) {
+        private static int woodBreaking(BlockPos pos, World world, IBlockState state, EntityPlayer player, int step) {
+        step ++;
+        //Iterator knownPosValue = knownPos.iterator();
+
+        /*while (knownPosValue.hasNext()) {
+            state.getBlock().dropBlockAsItem(world, (BlockPos) knownPosValue.next(), state, 1);
+            world.setBlockToAir((BlockPos) knownPosValue.next());
+            player.getHeldItemMainhand().damageItem(1, player);
+        }*/
 
             for (int y = 0; y <= 1; y++) {
                 for (int x = -1; x <= 1; x++) {
                     for (int z = -1; z <= 1; z++) {
                             if ((world.getBlockState(pos.add(x,y,z)).getBlock() == Blocks.LOG || world.getBlockState(pos.add(x,y,z)).getBlock() == Blocks.LOG2) && pos.add(x,y,z) != pos) {
-                                //System.out.println(pos.add(x,y,z).toString());
-
+                                if(step == 30) {
+                                    return step;
+                                }
                                 state.getBlock().dropBlockAsItem(world, pos.add(x,y,z), state, 1);
-
-                                //world.getBlockState(pos.add(x,y,z)).getBlock().breakBlock(world, pos.add(x,y,z), state);
-
                                 world.setBlockToAir(pos.add(x,y,z));
                                 player.getHeldItemMainhand().damageItem(1, player);
-                                woodBreakingRecursion(pos.add(x,y,z), world, state, player, step);
-                                if(step == 30) {
-                                    return;
-                                }
+                                //woodBreaking(pos.add(x,y,z), world, state, player, step, knownPos);
+                                step = woodBreaking(pos.add(x,y,z), world, state, player, step);
                             }
                         }
                     }
                 }
+            return step;
         }
 
         @SubscribeEvent
