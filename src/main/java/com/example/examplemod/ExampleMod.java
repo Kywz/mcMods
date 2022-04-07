2175aadd4b4bee1464ddc5debff63b0fdd5f6278
Danil Strelyaev <danil.strelyaev@gmail.com>
2022-04-07 19:07:10 +0300

    final version, complete

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -18,28 +18,8 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
 public class ExampleMod
 {
-    /*@Mod.EventBusSubscriber
-    public class EventsHandler {
-        @SubscribeEvent
-        public static void onBlockBreak(BlockEvent.BreakEvent event){
-            //event.getPlayer().jump();
-            event.getPlayer().addExperience(500);
-            if(event.getWorld().getBlockState(event.getPos()).getBlock() == Blocks.LOG2) {
-                event.getPlayer().jump();
-                System.out.println("hello block");
-            }
-        }
-
-        @SubscribeEvent
-        public void onJoin(EntityJoinWorldEvent e) {
-            if (e.getEntity() instanceof EntityPlayer) {
-                EntityPlayer player = (EntityPlayer) e.getEntity();
-                player.sendMessage(new TextComponentString("Hello, %p!".replace("%p", player.getName())));
-            }
-        }
-    }*/
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+    public static final String MODID = "trcp";
+    public static final String NAME = "Tree Capitator";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
@@ -53,8 +33,7 @@ public class ExampleMod
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        // some example code
-        //logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+
     }
 
 }
--- src/main/java/com/example/examplemod/TreeCapitatorEvents.java
+++ src/main/java/com/example/examplemod/TreeCapitatorEvents.java
@@ -1,22 +1,17 @@
 package com.example.examplemod;
 
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemAxe;
-import net.minecraft.item.ItemTool;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3i;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
-import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-
 import java.util.HashSet;
-import java.util.Iterator;
+
 
 @Mod.EventBusSubscriber
 public class TreeCapitatorEvents {
@@ -25,25 +20,12 @@ public class TreeCapitatorEvents {
         public static void onBlockBreaking(PlayerEvent.BreakSpeed event){
             event.getEntityPlayer().sendMessage(new TextComponentString(String.valueOf(event.getOriginalSpeed())));
                 if (event.getEntityPlayer().getHeldItemMainhand().getItem() instanceof ItemAxe) {
-                    event.getEntityPlayer().sendMessage(new TextComponentString("Axe have been used"));
                     if (event.getEntityPlayer().isSneaking() == true) {
-                        event.getEntityPlayer().sendMessage(new TextComponentString("Player is sneaking"));
                         if ( event.getEntityPlayer().getEntityWorld().getBlockState(event.getPos()).getBlock() == Blocks.LOG ||  event.getEntityPlayer().getEntityWorld().getBlockState(event.getPos()).getBlock() == Blocks.LOG2) {
-
                             HashSet<BlockPos> blocksToBeBroken = new HashSet<BlockPos>();
                             blocksToBeBroken.add(event.getPos());
-                            blocksToBeBroken = woodBreakingRecursionCount(event.getPos(),event.getEntityPlayer().getEntityWorld(),event.getState(),blocksToBeBroken);
-
+                            blocksToBeBroken = woodBreakingRecursionCount(event.getPos(), event.getEntityPlayer().getEntityWorld(), blocksToBeBroken);
                             event.setNewSpeed(event.getOriginalSpeed() / blocksToBeBroken.size());
-                            event.getEntityPlayer().sendMessage(new TextComponentString("HashSet Size:"+blocksToBeBroken.size()));
-                            event.getEntityPlayer().sendMessage(new TextComponentString("Current speed "+event.getNewSpeed()));
-
-                            event.getEntityPlayer().sendMessage(new TextComponentString("Current result "+event.getResult()));
-
-                            //event.
-
-                            //woodBreaking(event.getPos(), event.getEntityPlayer().getEntityWorld(), event.getState(), event.getEntityPlayer(), blocksToBeBroken);
-                            //event.getEntityPlayer().sendMessage(new TextComponentString("Block broken"));
                     }
                 }
             }
@@ -52,34 +34,19 @@ public class TreeCapitatorEvents {
 
         @SubscribeEvent
         public static void onBlockBreak(BlockEvent.BreakEvent event) {
-            //event.getEntityPlayer().sendMessage(new TextComponentString(String.valueOf(event.getOriginalSpeed())));
             if (event.getPlayer().getHeldItemMainhand().getItem() instanceof ItemAxe) {
-                event.getPlayer().sendMessage(new TextComponentString("Axe have been used"));
                 if (event.getPlayer().isSneaking() == true) {
-                    event.getPlayer().sendMessage(new TextComponentString("Player is sneaking"));
                     if ( event.getWorld().getBlockState(event.getPos()).getBlock() == Blocks.LOG ||  event.getWorld().getBlockState(event.getPos()).getBlock() == Blocks.LOG2) {
-
-                        //HashSet<BlockPos> blocksToBeBroken = new HashSet<BlockPos>();
-                        //blocksToBeBroken.add(event.getPos());
-                       //blocksToBeBroken = woodBreakingRecursionCount(event.getPos(),event.getEntityPlayer().getEntityWorld(),event.getState(),blocksToBeBroken);
-
-                        //event.setNewSpeed(event.getOriginalSpeed() / blocksToBeBroken.size());
-                        //event.getEntityPlayer().sendMessage(new TextComponentString("HashSet Size:"+blocksToBeBroken.size()));
-                        //event.getEntityPlayer().sendMessage(new TextComponentString("Current speed"+event.getNewSpeed()));
-
                         HashSet<BlockPos> blocksToBeBroken = new HashSet<BlockPos>();
                         blocksToBeBroken.add(event.getPos());
-                        //woodBreaking(event.getPos(), event.getWorld(), event.getState(), event.getPlayer(), woodBreakingRecursionCount(event.getPos(),event.getWorld(),event.getState(),blocksToBeBroken));
-                        woodBreaking(event.getPos(), event.getWorld(), event.getState(), event.getPlayer(), 0);
-                        event.getPlayer().sendMessage(new TextComponentString("Block broken"));
-
+                        woodBreaking(event.getWorld(), event.getPlayer(), woodBreakingRecursionCount(event.getPos(),event.getWorld(), blocksToBeBroken));
                     }
                 }
             }
         }
 
 
-    private static HashSet woodBreakingRecursionCount(BlockPos pos, World world, IBlockState state, HashSet<BlockPos> knownPos) {
+    private static HashSet woodBreakingRecursionCount(BlockPos pos, World world, HashSet<BlockPos> knownPos) {
         for (int y = 0; y <= 1; y++) {
             for (int x = -1; x <= 1; x++) {
                 for (int z = -1; z <= 1; z++) {
@@ -89,7 +56,7 @@ public class TreeCapitatorEvents {
                         }
                         if(!(knownPos.contains(pos.add(x,y,z)))) {
                             knownPos.add(pos.add(x,y,z));
-                            knownPos = woodBreakingRecursionCount(pos.add(x,y,z), world, state, knownPos);
+                            knownPos = woodBreakingRecursionCount(pos.add(x,y,z), world, knownPos);
                         }
                     }
                 }
@@ -98,41 +65,10 @@ public class TreeCapitatorEvents {
         return knownPos;
     }
 
-        //private static void woodBreaking(BlockPos pos, World world, IBlockState state, EntityPlayer player, HashSet knownPos, int step) {
-        private static int woodBreaking(BlockPos pos, World world, IBlockState state, EntityPlayer player, int step) {
-        step ++;
-        //Iterator knownPosValue = knownPos.iterator();
-
-        /*while (knownPosValue.hasNext()) {
-            state.getBlock().dropBlockAsItem(world, (BlockPos) knownPosValue.next(), state, 1);
-            world.setBlockToAir((BlockPos) knownPosValue.next());
-            player.getHeldItemMainhand().damageItem(1, player);
-        }*/
-
-            for (int y = 0; y <= 1; y++) {
-                for (int x = -1; x <= 1; x++) {
-                    for (int z = -1; z <= 1; z++) {
-                            if ((world.getBlockState(pos.add(x,y,z)).getBlock() == Blocks.LOG || world.getBlockState(pos.add(x,y,z)).getBlock() == Blocks.LOG2) && pos.add(x,y,z) != pos) {
-                                if(step == 30) {
-                                    return step;
-                                }
-                                state.getBlock().dropBlockAsItem(world, pos.add(x,y,z), state, 1);
-                                world.setBlockToAir(pos.add(x,y,z));
-                                player.getHeldItemMainhand().damageItem(1, player);
-                                //woodBreaking(pos.add(x,y,z), world, state, player, step, knownPos);
-                                step = woodBreaking(pos.add(x,y,z), world, state, player, step);
-                            }
-                        }
-                    }
-                }
-            return step;
-        }
-
-        @SubscribeEvent
-        public static void onJoin(EntityJoinWorldEvent e) {
-            if (e.getEntity() instanceof EntityPlayer) {
-                EntityPlayer player = (EntityPlayer) e.getEntity();
-                player.sendMessage(new TextComponentString("Hello, %p!".replace("%p", player.getName())));
+        private static void woodBreaking(World world, EntityPlayer player, HashSet<BlockPos> knownPos) {
+            for (BlockPos blocPos: knownPos) {
+                world.destroyBlock(blocPos, true);
+                player.getHeldItemMainhand().damageItem(1, player);
             }
         }
     }
