8e0fe73512b506532e3eec88ec3b4953bb06c0ff
Danil Strelyaev <danil.strelyaev@gmail.com>
2022-04-07 19:17:00 +0300

    first commit, grounds for loot box mod

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -15,20 +15,6 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
 public class ExampleMod
 {
-    public class EventsHandler {
-        @SubscribeEvent
-        public void onBlockBreak(BlockEvent.BreakEvent event){
-
-        }
-
-        @SubscribeEvent
-        public void onJoin(EntityJoinWorldEvent e) {
-            if (e.getEntity() instanceof EntityPlayer) {
-                EntityPlayer player = (EntityPlayer) e.getEntity();
-                player.sendMessage(new TextComponentString("Hello, %p!".replace("%p", player.getName())));
-            }
-        }
-    }
     public static final String MODID = "examplemod";
     public static final String NAME = "Example Mod";
     public static final String VERSION = "1.0";
