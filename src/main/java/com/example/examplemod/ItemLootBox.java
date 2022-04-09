cc72c36332c6fc9cc123be8817681cd633303262
Danil Strelyaev <danil.strelyaev@gmail.com>
2022-04-09 17:09:40 +0300

    v 1.0.0 final version

--- src/main/java/com/example/examplemod/ItemLootBox.java
+++ src/main/java/com/example/examplemod/ItemLootBox.java
@@ -1,60 +1,52 @@
 package com.example.examplemod;
-
-import net.minecraft.client.tutorial.Tutorial;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
-import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.*;
 import net.minecraft.world.World;
-import net.minecraftforge.registries.IForgeRegistryEntry;
-import sun.security.util.Length;
+
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class ItemLootBox extends Item {
-    int[] weightRandomList = {10, 24, 55, 87, 100};
+    float[] weightRandomList = {0.8f, 2.0f, 0.7f, 0.2f};
     List<Item> itemList = new ArrayList<Item>();
     Random randomGenerator = new Random();
-        //itemList.add(0, Items.ARROW);
-        //itemList.add(1, Items.BED);
-        //itemList.add(2, Items.BOW);
-        //itemList.add(3, Items.BRICK);
     public ItemLootBox() {
         this.setCreativeTab(ExampleMod.CTAB);
         this.setRegistryName("lootbox");
         this.setUnlocalizedName("lootbox");
-        //float[] weightRandomList = {10, 24, 55, 87};
-        //List<Item> itemList = new ArrayList<Item>();
+        this.setMaxStackSize(1);
         itemList.add(0, Items.ARROW);
         itemList.add(1, Items.BED);
         itemList.add(2, Items.BOW);
         itemList.add(3, Items.BRICK);
-
     }
 
     public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand hand) {
+        float weightSum = 0;
+        float currentItem = 0;
 
+        for (float currentWeight: weightRandomList) {
+                weightSum += currentWeight;
+        }
 
-        int generatedValue = randomGenerator.nextInt(weightRandomList[(itemList.size()-1)]);
-        System.out.println(generatedValue);
-        for(int itemIter = 1; itemIter <= itemList.size()-1; itemIter++) {
-            if(weightRandomList[itemIter] <= generatedValue && generatedValue <= weightRandomList[itemIter+1]) {
-                System.out.println(generatedValue);
-                //playerIn.spawnRunningParticles();
-                //playerIn.getPosition()
-                playerIn.getActiveItemStack().setCount(5);
+        float generatedValue = randomGenerator.nextFloat();
+        weightSum *= generatedValue;
+
+        for(int itemIter = 0; itemIter < itemList.size(); itemIter++) {
+            currentItem += weightRandomList[itemIter];
+            if (currentItem > weightSum) {
+                worldIn.spawnParticle(EnumParticleTypes.EXPLOSION_HUGE  ,(double) playerIn.getPosition().getX(),(double) playerIn.getPosition().getY(),(double) playerIn.getPosition().getZ(), 0.25,0.15,0);
+                worldIn.playSound(playerIn, playerIn.getPosition(),SoundEvent.REGISTRY.getObject(new ResourceLocation("entity.experience_orb.pickup")), SoundCategory.PLAYERS, 1.0f, 1.0f);
+                playerIn.getHeldItem(hand).shrink(1);
+                playerIn.addItemStackToInventory(new ItemStack(itemList.get(itemIter)));
                 break;
             }
         }
-        //playerIn.jump();
         return super.onItemRightClick(worldIn, playerIn, hand);
     }
 }
